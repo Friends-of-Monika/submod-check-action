f039e7eb23222ce3dbde232f5b7b495c3c297c18
dreamscached <dreamscache.d@gmail.com>
2022-06-20 07:48:13 +0200

    Fix tag fetch

--- steps/store-mas-release-or-fallback.sh
+++ steps/store-mas-release-or-fallback.sh
@@ -9,7 +9,7 @@ mas_release="$1"
 
 if [ -z "$mas_release" ]; then
     echo "::set-output name=mas-release::$(curl -sL "https://api.github.com/repos/monika-after-story/monikamoddev/releases/latest" | \
-        perl -lne 'print $1 if /"browser_download_url": "(.+?-Mod\.zip)"/')"
+        perl -lne 'print $1 if /"tag_name": "([^"]+)"/')"
 else
     echo "::set-output name=mas-release::$mas_release"
 fi
