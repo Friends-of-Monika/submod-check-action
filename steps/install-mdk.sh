058f0bda41ecda59239ab9d798777bef09e873bd
dreamscached <dreamscache.d@gmail.com>
2022-07-10 09:38:50 +0200

    Suppress tar output

--- steps/install-mdk.sh
+++ steps/install-mdk.sh
@@ -21,7 +21,7 @@ docker run -d --rm --name mdk ghcr.io/friends-of-monika/mas-mdk:latest \
     /bin/sh -c "sleep 999999999"
 
 docker export mdk -o "$temp/mdk.tar"
-tar -xvf "$temp/mdk.tar" --directory "$temp" mdk
+tar -xf "$temp/mdk.tar" --directory "$temp" mdk
 
 mv "$temp/mdk/mas" "$mas"
 mv "$temp/mdk/renpy" "$renpy"
